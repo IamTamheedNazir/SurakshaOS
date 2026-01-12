3fcbc752c38afe26d395013655e371fca7c84d6b
Tamheed Nazir <62136544+IamTamheedNazir@users.noreply.github.com>
2026-01-12 13:13:48 +0530

    feat: Update App.js to use ProfessionalHomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,9 @@ import './App.css';
 
 // Layout Components
 import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
 
 // Pages
-import NewHomePage from './pages/NewHomePage';
-import PackagesPage from './pages/PackagesPage';
-import PackageDetailPage from './pages/PackageDetailPage';
-import BookingPage from './pages/BookingPage';
-import TrackingPage from './pages/TrackingPage';
-import LoginPage from './pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-import DashboardPage from './pages/dashboard/DashboardPage';
-import VendorDashboardPage from './pages/vendor/VendorDashboardPage';
-import VendorProfilePage from './pages/VendorProfilePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import NotFoundPage from './pages/NotFoundPage';
-
-// Protected Route Component
-import ProtectedRoute from './components/auth/ProtectedRoute';
+import ProfessionalHomePage from './pages/ProfessionalHomePage';
 
 function App() {
   return (
@@ -31,54 +15,11 @@ function App() {
       <main className="main-content">
         <Routes>
           {/* Public Routes */}
-          <Route path="/" element={<NewHomePage />} />
-          <Route path="/packages" element={<PackagesPage />} />
-          <Route path="/packages/:id" element={<PackageDetailPage />} />
-          <Route path="/vendors/:vendorId" element={<VendorProfilePage />} />
-          <Route path="/track" element={<TrackingPage />} />
-          <Route path="/track/:bookingNumber" element={<TrackingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <route path="/" element={<ProfessionalHomePage />} />
           
-          {/* Auth Routes */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          
-          {/* Protected Customer Routes */}
-          <Route
-            path="/dashboard/*"
-            element={
-              <ProtectedRoute>
-                <DashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* Protected Vendor Routes */}
-          <Route
-            path="/vendor/*"
-            element={
-              <ProtectedRoute requiredRole="vendor">
-                <VendorDashboardPage />
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* Booking Flow */}
-          <Route
-            path="/booking/:packageId"
-            element={
-              <ProtectedRoute>
-                <BookingPage />
-              </ProtectedRoute>
-            }
-          />
-          
-          {/* 404 Page */}
-          <Route path="*" element={<NotFoundPage />} />
+          {/* More routes will be added */}
         </Routes>
       </main>
-      <Footer />
     </div>
   );
 }
