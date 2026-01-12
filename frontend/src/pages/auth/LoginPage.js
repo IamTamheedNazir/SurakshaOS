efdb2b85ce903e231dd505b3785d16264f579887
Tamheed Nazir <62136544+IamTamheedNazir@users.noreply.github.com>
2026-01-12 11:25:54 +0530

    fix: Remove unused toast import from LoginPage

--- frontend/src/pages/auth/LoginPage.js
+++ frontend/src/pages/auth/LoginPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
-import { toast } from 'react-toastify';
 import useAuthStore from '../../store/authStore';
 import './LoginPage.css';
 
